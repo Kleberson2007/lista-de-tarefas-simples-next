9ba64ccdc6a1e8bdc8f5bc7afb54da4bbc9b0fe6
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-09-19 14:34:33 -0300

    kklç

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,48 @@
-import { use, useState } from "react";
+"use client"
+import React, { FormEvent, useState } from "react";
 import "./globals.css";
 
-const [tarefas, setTarefas] = useState(["Estudar contador", "Estudar lista de tarefas"])
-const [nova, setNova] = ("")
-
-function adicionar() {
-  
-}
-
 export default function Home() {
+  const [tarefas, setTarefas] = useState(["Estudar contador", "Estudar lista de tarefas"]);
+  const [nova, setNova] = useState("");
+
+  function adicionar(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setTarefas([...tarefas, nova]);
+    setNova("");
+  }
+
+  function deletar(i: number){
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(i, 1);
+    setTarefas(novasTarefas)
+  }
+
   return (
-    <div>
-      <h1>lista de tarefas</h1>
+    <div id="lista">
+      <header>
+        <h1>lista de tarefas</h1>
+      </header>
       <form onSubmit={adicionar}>
         <input
         type="text"
-        placeholder="diite sua tarefa"
-        value={nova} />
-        <button>adicionar</button>
+        placeholder="digite sua tarefa..."
+        value={nova}
+        onChange={e => setNova(e.target.value)} />
+        <button type="submit">adicionar</button>
       </form>
+      <ul>
+        {tarefas.map((tarefa, index) => (
+          <li key={index}>
+            <div id="lis">
+              <div id="texto">
+                {tarefa}
+              </div>
+              <button className="botãodeletar" onClick={() => deletar(index)}>deletar</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
